92a3588056810479bd17ea2225f65b01b0832424
mohd-musheer <musheerayan@gmail.com>
2026-05-30 23:17:10 +0530

    running mcp

--- Projects/MCP/client.py
+++ Projects/MCP/client.py
@@ -1,6 +1,8 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
 import asyncio
+from langgraph.prebuilt import create_react_agent
 from langchain.chat_models import init_chat_model
 import os
 from dotenv import load_dotenv
@@ -12,25 +14,32 @@ async def main():
         {
             "Math": {
                 "command": "python",
-                # FIX 1: Provide the correct relative path to the maths server
                 "args": ["Projects/MCP/maths_server.py"], 
                 "transport": "stdio"
             },
             "weather": {
-                "transport": "sse",
-                # FIX 2: Ensure this points exactly to /sse, not /mcp
-                "url": "http://127.0.0.1:8000/sse" 
+                "url":"http://127.0.0.1:8000/mcp",
+                "transport": "streamable_http" 
             }
         }
     )
     os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
     
     tools = await client.get_tools()
-    model = init_chat_model("groq:openai/gpt-oss-120b")
+    model = init_chat_model(
+        "groq:openai/gpt-oss-120b"
+    )
     agent = create_react_agent(model, tools)
     
-    math_response = await agent.invoke(
-        {"messages":[{"role":"user","content":"calculate 6 plus 9 into 8 and also what is wether in Nandgaon Peth currently"}]}
+    math_response = await agent.ainvoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "calculate 6 plus 9 into 8 and also what is weather in Nandgaon Peth currently"
+                }
+            ]
+        }
     )
     
     print("response : ", math_response["messages"][-1].content)
